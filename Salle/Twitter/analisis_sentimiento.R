7c822056437850477617fa33661d124ec9f1f7e0
Elina Gómez <elinagomezbonaglia@gmail.com>
2025-02-10 09:59:15 -0300

    sent

---
+++ Salle/Twitter/analisis_sentimiento.R
@@ -0,0 +1,52 @@
+
+
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+
+
+salle_sent <- read_csv("Salle/Twitter/salle_sent.csv")
+
+
+
+evo_sent <- salle_sent %>%
+  filter(Anio!=2025)%>%
+  group_by(Anio, sentimiento) %>%
+  summarise(conteo_posteos = n(), .groups = "drop") %>%
+  group_by(Anio) %>%
+  mutate(proporcion = round(conteo_posteos / sum(conteo_posteos)*100,1)) %>%
+  mutate(Anio = as.factor(Anio))
+
+
+grafico_sent <- ggplot(evo_sent, aes(x = Anio, y = proporcion, 
+                                     color = sentimiento, 
+                                     group = sentimiento,
+                                     text = paste0("Sentimiento: ", sentimiento, "<br>",
+                                                   "Año: ", Anio, "<br>",
+                                                   "Porcentaje: ", paste(proporcion))) +
+  geom_line(size = 1) + 
+  geom_point(size = 2) +  
+  labs(title = "Evolución de posteos por sentimiento y año") +
+  theme_minimal(base_size = 14) +  
+  theme(legend.position = "bottom",  
+        plot.title = element_text(hjust = 0.5, face = "bold"),  
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        axis.title = element_blank())  
+
+grafico_interactivo_sent <- ggplotly(grafico_sent, tooltip = "text") %>%
+  layout(legend = list(orientation = "h",  
+                       x = 0.5,  
+                       y = -0.2,  
+                       xanchor = "center", 
+                       yanchor = "top",
+                       title = list(text = "")))  
+
+grafico_interactivo_sent
+
+
+
+
+
+
